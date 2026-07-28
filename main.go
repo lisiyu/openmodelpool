b9b118df3d2fd0ca4d7128b4e30fb8a5da44c9c1
licha <157621790@qq.com>
2026-07-29 01:12:45 +0800

    chore: bump AppVersion to 4.1.9 (release gate for domain-binding deploy)

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-var AppVersion string = "4.1.8"
+var AppVersion string = "4.1.9"
 
 // checkAndFixFilePermissions ensures sensitive files have restricted permissions. (SA-08)
 func checkAndFixFilePermissions(paths []string) {
