3592b5a64fd8fa2032b6104ff4e1df5696b289b7
lisiyu <lisiyu@users.noreply.github.com>
2026-08-05 01:15:25 +0800

    bump: v4.2.8

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-var AppVersion string = "4.2.7"
+var AppVersion string = "4.2.8"
 
 // checkAndFixFilePermissions ensures sensitive files have restricted permissions. (SA-08)
 func checkAndFixFilePermissions(paths []string) {
