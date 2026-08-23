6ae00170cc52a96a912ade4d8956dd969cb8a476
lisiyu <lisiyu@users.noreply.github.com>
2026-08-23 09:45:53 +0800

    release: v4.5.27

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-var AppVersion string = "4.5.26"
+var AppVersion string = "4.5.27"
 
 // checkAndFixFilePermissions ensures sensitive files have restricted permissions. (SA-08)
 func checkAndFixFilePermissions(paths []string) {
